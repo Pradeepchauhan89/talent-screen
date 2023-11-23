4912da1a530802b052c0580407717e0a2e384c18
Pradeep <pradeep@incaendo.com>
2023-11-23 16:43:47 +0530

    perf: make execution controller more dynamic

--- src/controllers/ExecuteController.py
+++ src/controllers/ExecuteController.py
@@ -17,31 +17,15 @@ class ExecuteController:
 
   def execute(self, payload: AgentSchema):
     try:
-      payload = payload.dict()
-      for param in payload['inputs']:
-        # get your input parameter here
-        logger.info(param['name'])
-        logger.info(param['data'])
-
+      #add_input_here
+      resp = ''
       # resp =  callYourAgent()
+      #add_output_here
+      # Call spritz API
+      #add_callback_here
 
-      # Call spritz TS API
-      call_webhook_with_success({
-          "status": "completed",
-          "data": {
-              "title": "Write your title here",
-              "info": "Write some information here",
-              "output": {
-                  "name": "Any suitable name",
-                  "type": "list", # check A2A documentation
-                  "data": "agent response"
-              }
-          }
-      })
-      logger.info('Function execute: Execution complete', "data that you want to log here")
-      # logger.info('Function execute: Execution complete', resp)
-      return { "summary": "Agent execution has been completed.", "detail": "response that you want to return in api response" }
-      # return { "summary": "Agent execution has been completed.", "detail": resp }
+      logger.info('Function execute: Execution complete', resp)
+      return { "summary": "Agent execution has been completed.", "detail": resp }
     except Exception as e:
       logger.error('Function execute: error', e)
       raise call_webhook_with_error(str(e), 500)
